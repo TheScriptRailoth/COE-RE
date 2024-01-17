b9b7fd27a42e076959bedb64d4cc3c2156588909
Ashutosh <am3718440@gmail.com>
2024-01-17 19:20:15 +0530

    COE_RE: Added team section and Gallery
    Signed-off-by: Ashutosh <am3718440@gmail.com>

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var members = document.querySelectorAll('.member');
+
+    members.forEach(function (member) {
+        member.addEventListener('click', function () {
+            var profilePath = member.getAttribute('data-profile');
+            window.location.href = profilePath;
+        });
+    });
+    
+    const btn1 = document.getElementById("btn1");
+    const btn2 = document.getElementById("btn2");
+
+    const msgContainer = document.getElementById("msgContainer");
+    const msgContainer2 = document.getElementById("msgContainer2");
+
+    btn1.style.backgroundColor = "blue";
+    btn1.style.color = "white";
+    let msg = true;
+
+    btn2.addEventListener("click", () => {
+        if (msg === true) {
+            btn1.style.backgroundColor = "white";
+            btn1.style.color = "black";
+            btn2.style.backgroundColor = "blue";
+            btn2.style.color = "white";
+            msgContainer.style.display = "none";
+            msgContainer2.style.display = "block";
+            msg = false;
+        }
+    });
+
+    btn1.addEventListener("click", () => {
+        if (msg === false) {
+            btn2.style.backgroundColor = "white";
+            btn2.style.color = "black";
+            btn1.style.backgroundColor = "blue";
+            btn1.style.color = "white";
+            msgContainer2.style.display = "none";
+            msgContainer.style.display = "block";
+            msg = true;
+        }
+    });
+});
